ff1686cafbe18e59b872f42c3518f79e9ed4c53d
Ritesh Gharat <75104422+imritpro@users.noreply.github.com>
2021-03-08 19:34:19 +0530

    Updated sw

--- calculator/sw.js
+++ calculator/sw.js
@@ -1,61 +1,71 @@
-
 var CACHE_VERSION = 'app-v1';
 var CACHE_FILES = [
     '/',
-    'app.html',
+    'index.html',
     'content/style.css',
     'content/main.js',
     'images/logo.png',
     'images/ellipsis-v-solid.svg',
     'images/github-brands.svg',
     'images/times-circle-regular.svg',
-    'manifest.json',
+    'logo/icon48.png',
+    'logo/icon96.png',
+    'logo/icon144.png',
+    'logo/icon196.png',
+    'logo/icon512.png',
+    'manifest.json'
 ];
 
-// This is the "Offline page" service worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE)
-    .then((cache) => cache.add(offlineFallbackPage))
+    caches.open(CACHE_VERSION)
+    .then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(CACHE_FILES);
+    })
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
+self.addEventListener('fetch', function(event) {
+  let online = navigator.onLine
+  if (!online) {
+    event.respondWith(
+      caches.match(event.request).then(function(res) {
+        if (res) {
+          return res;
+        }
+        requestBackend(event);
+      })
+    )
+  }
+});
+
+function requestBackend(event) {
+  var url = event.request.clone();
+  return fetch(url).then(function(res) {
+    //if not a valid response send the error
+    if (!res || res.status !== 200 || res.type !== 'basic') {
+      return res;
+    }
+
+    var response = res.clone();
+
+    caches.open(CACHE_VERSION).then(function(cache) {
+      cache.put(event.request, response);
+    });
+
+    return res;
+  })
 }
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(key, i) {
+        if (key !== CACHE_VERSION) {
+          return caches.delete(keys[i]);
         }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
+      }))
+    })
+  )
 });
